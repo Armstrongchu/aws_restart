f3006e5a6f0c5eed787d0d2c2bd6faa4c90964b6
unknown <armstrong_chu@hotmai.com>
2023-03-01 13:52:20 +1100

    1/03/2023

--- msg.py
+++ msg.py
@@ -1,6 +1,5 @@
-import sys
-sName = input("Name:")
-print(type(sName))
-sType=type(sName)
-print(type(sType))
-print("display the type of input   "+str(sType))
+list=[1,2,3,4,5,6,7,8]
+print(list[7])
+print(list[8:1:-1])
+print(list[7])
+print(list[-1:999:1])
